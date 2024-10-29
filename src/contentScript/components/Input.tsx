6478d6e63e522007e0c53b4c1b553ddc0f6304b5
awyera <awyerura@gmail.com>
2024-10-30 06:18:20 +0900

    fix: prevent YouTube shortcuts from triggering during input

--- src/contentScript/components/Input.tsx
+++ src/contentScript/components/Input.tsx
@@ -1,25 +1,37 @@
-import type { ChangeEvent, ComponentProps } from "react";
-import { twMerge } from "tailwind-merge";
+import type { ChangeEvent, ComponentProps, KeyboardEvent } from 'react';
+import { twMerge } from 'tailwind-merge';
 
-interface Props extends Omit<ComponentProps<"input">, "value" | "onChange"> {
+interface Props extends Omit<ComponentProps<'input'>, 'value' | 'onChange'> {
   value: string;
   onChange: (value: string) => void;
 }
 
-export function Input({ className, value, onChange, ...props }: Props) {
+export function Input({ className, value, onChange, onKeyDown, onKeyUp, ...props }: Props) {
   function handleChange(e: ChangeEvent<HTMLInputElement>) {
     const value = e.currentTarget.value;
 
     onChange(value);
   }
 
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    onKeyDown?.(e);
+    e.stopPropagation();
+  };
+
+  const handleKeyUp = (e: KeyboardEvent<HTMLInputElement>) => {
+    onKeyUp?.(e);
+    e.stopPropagation();
+  };
+
   return (
     <input
       {...props}
-      className={twMerge("w-full rounded border border-gray-500 px-1 py-1 text-base leading-normal", className)}
+      className={twMerge('w-full rounded border border-gray-500 px-1 py-1 text-base leading-normal', className)}
       type="text"
       value={value}
       onChange={handleChange}
+      onKeyDown={handleKeyDown}
+      onKeyUp={handleKeyUp}
     />
   );
 }
