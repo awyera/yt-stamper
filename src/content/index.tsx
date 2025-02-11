c3482e2a3e86d749db97d4ef7c23605362dea41f
awyera <awyerura@gmail.com>
2025-02-12 00:08:34 +0900

    Fix: missing display without chat or playlist

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -43,25 +43,63 @@ class YTStamperElement extends HTMLElement {
   }
 }
 
-const observer = new MutationObserver((_mutations, obs) => {
-  const container = document.querySelector('#secondary-inner');
-  if (container) {
-    const chat = document.querySelector('#chat');
-    const playlist = document.querySelector('#playlist');
-    // chat または playlist がレンダリングされたあとに yt-stamper を追加する
-    if (chat || playlist?.getAttribute('hidden') === null) {
-      if (!customElements.get(ELEMENT_NAME)) {
-        customElements.define(ELEMENT_NAME, YTStamperElement);
-      }
-      const ytStamperElement = document.createElement(ELEMENT_NAME);
+let currentId = '';
 
-      container.prepend(ytStamperElement);
-      obs.disconnect();
+const handleHistoryChange = (message: { url: string }) => {
+  if (currentId !== message.url) {
+    currentId = message.url;
+
+    const container = document.querySelector('#secondary-inner');
+    const ytStamperElement = document.querySelector(ELEMENT_NAME);
+    if (container && ytStamperElement) {
+      container.removeChild(ytStamperElement);
     }
-  }
-});
 
-observer.observe(document.body, {
-  childList: true,
-  subtree: true,
-});
+    const observer = new MutationObserver((_mutations, obs) => {
+      const container = document.querySelector('#secondary-inner');
+      if (container) {
+        // yt-stamper が既に存在する場合は何もしない
+        if (document.querySelector(ELEMENT_NAME)) {
+          obs.disconnect();
+          return;
+        }
+
+        const chat = document.querySelector('#chat');
+        const playlist = document.querySelector('#playlist');
+        // chat または playlist がレンダリングされたあとに yt-stamper を追加する
+        if (chat || playlist?.getAttribute('hidden') === null) {
+          const ytStamperElement = document.createElement(ELEMENT_NAME);
+          container.prepend(ytStamperElement);
+          obs.disconnect();
+          clearTimeout(timerId);
+        }
+      }
+    });
+
+    const timerId = setTimeout(() => {
+      const container = document.querySelector('#secondary-inner');
+      if (container) {
+        // yt-stamper が既に存在する場合は何もしない
+        if (document.querySelector(ELEMENT_NAME)) {
+          observer.disconnect();
+          clearTimeout(timerId);
+          return;
+        }
+
+        const ytStamperElement = document.createElement(ELEMENT_NAME);
+        container.prepend(ytStamperElement);
+        observer.disconnect();
+      }
+    }, 1000);
+
+    observer.observe(document.body, {
+      childList: true,
+      subtree: true,
+    });
+  }
+};
+
+customElements.define(ELEMENT_NAME, YTStamperElement);
+
+chrome.runtime.onMessage.addListener(handleHistoryChange);
+handleHistoryChange({ url: window.location.href });
