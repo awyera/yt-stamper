9eda9d1c5727d342b7e4fc0d31016fb2b8060c6e
awyera <awyerura@gmail.com>
2025-03-02 15:27:57 +0900

    feat: update desing of options page

--- src/options/components/ListItem.tsx
+++ src/options/components/ListItem.tsx
@@ -18,13 +18,12 @@ type Props = {
 export const ListItem = ({ className, videoId, videoTimestamps, onDelete }: Props) => {
   const [open, setOpen] = useState(false);
 
-  const watchUrl = (id: string, time?: string): string => {
+  const watchUrl = (time?: string): string => {
+    const url = `https://www.youtube.com/watch?v=${videoId}`;
     if (!time) {
-      return `https://www.youtube.com/watch?v=${id}`;
+      return url;
     }
-
-    const sec = parseTime(time);
-    return `https://www.youtube.com/watch?v=${id}&t=${sec}`;
+    return `${url}&t=${parseTime(time)}`;
   };
 
   const handleClick = () => {
@@ -33,12 +32,12 @@ export const ListItem = ({ className, videoId, videoTimestamps, onDelete }: Prop
 
   const handleDelete = () => {
     onDelete(videoId);
-  }
+  };
 
   return (
-    <div className={twMerge('flex items-start justify-start gap-x-4 overflow-hidden', className)}>
-      <div>
-        <a href={watchUrl(videoId)} target="_blank" rel="noreferrer noopener">
+    <div className={twMerge('flex items-start justify-start text-sm gap-x-4 overflow-hidden', className)}>
+      <div className="shrink-0">
+        <a href={watchUrl()} target="_blank" rel="noreferrer noopener">
           <img
             className="h-[180px] w-[320px] object-cover"
             src={`https://i.ytimg.com/vi/${videoId}/mqdefault.jpg`}
@@ -49,17 +48,22 @@ export const ListItem = ({ className, videoId, videoTimestamps, onDelete }: Prop
         </a>
       </div>
       <div>
-        <p className="font-bold mb-2">{videoTimestamps.videoDetails.title}</p>
-        <a
-          className="text-blue-500 underline"
-          href={`https://www.youtube.com/channel/${videoTimestamps.videoDetails.channelId}`}
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          {videoTimestamps.videoDetails.author}
+        <a className="text-base font-bold mb-2" href={watchUrl()}>
+          {videoTimestamps.videoDetails.title}
         </a>
-        <p>{format(videoTimestamps.videoDetails.publishedAt, 'yyyy/MM/dd HH:mm')}</p>
-        <Button className="" variant="white" onClick={handleClick}>
+        <div>
+          <a
+            className="text-blue-500 underline"
+            href={`https://www.youtube.com/channel/${videoTimestamps.videoDetails.channelId}`}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            {videoTimestamps.videoDetails.author}
+          </a>
+          <span className="mx-2">•</span>
+          <span>{format(videoTimestamps.videoDetails.publishedAt, 'yyyy/MM/dd HH:mm')} 投稿</span>
+        </div>
+        <Button className="mt-2" variant="white" onClick={handleClick}>
           {videoTimestamps.list.length} 個のタイムスタンプ
           {open ? <ChevronUp size="1em" /> : <ChevronDown size="1em" />}
         </Button>
@@ -71,7 +75,7 @@ export const ListItem = ({ className, videoId, videoTimestamps, onDelete }: Prop
                   <span>
                     <a
                       className="text-blue-500 underline"
-                      href={watchUrl(videoId, item.time)}
+                      href={watchUrl(item.time)}
                       target="_blank"
                       rel="noreferrer noopener"
                     >
@@ -85,7 +89,7 @@ export const ListItem = ({ className, videoId, videoTimestamps, onDelete }: Prop
           </div>
         )}
       </div>
-      <Button className="ml-auto" variant="danger" onClick={handleDelete}>
+      <Button className="ml-auto shrink-0" variant="danger" onClick={handleDelete}>
         削除
       </Button>
     </div>
